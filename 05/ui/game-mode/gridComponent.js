2c2411b36261e0b185267ef4b2478eb367dc65ef
victo <victor.kachin@gmail.com>
2026-01-11 18:00:08 +0300

    adding coordinate to playing cella

--- 05/ui/game-mode/gridComponent.js
+++ 05/ui/game-mode/gridComponent.js
@@ -1,17 +1,27 @@
+import { getGooglePosition, getGridSize } from '../../state/data.js'
+
 export const Grid = () => {
-  const element = document.createElement("table");
+	const element = document.createElement('table')
 
-  for (let y = 0; y < 4; y++) {
-    const row = document.createElement("tr");
+	const gridSize = getGridSize()
+	const googlePosition = getGooglePosition()
 
-    for (let x = 0; x < 4; x++) {
-      const cell = document.createElement("td");
-      cell.append(x, "-", y);
-      row.append(cell);
-    }
+	for (let y = 0; y < gridSize.rowCount; y++) {
+		const row = document.createElement('tr')
 
-    element.append(row);
-  }
+		for (let x = 0; x < gridSize.columnCount; x++) {
+			const cell = document.createElement('td')
+			// cell.append(x, '-', y)
 
-  return element;
-};
+			if (x === googlePosition.x && y === googlePosition.y) {
+				cell.append('G')
+			}
+
+			row.append(cell)
+		}
+
+		element.append(row)
+	}
+
+	return element
+}
