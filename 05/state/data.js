97c51999a38853ab9c07f642ebf68812db63555c
victo <victor.kachin@gmail.com>
2026-01-15 01:32:21 +0300

    fix

--- 05/state/data.js
+++ 05/state/data.js
@@ -1,52 +1,52 @@
-import { GAME_STATUSES } from "./GAME_STATUSES.js";
+import { GAME_STATUSES } from './GAME_STATUSES.js'
 
 const _state = {
-  status: GAME_STATUSES.SETTINGS,
-  settings: {
-    gridSize: {
-      rowCount: 4,
-      columnCount: 4,
-    },
-  },
-  position: {
-    google: { x: 0, y: 0 },
-    player1: { x: 3, y: 3 },
-    player2: { x: 2, y: 2 },
-  },
-};
+	status: GAME_STATUSES.SETTINGS,
+	settings: {
+		gridSize: {
+			rowCount: 4,
+			columnCount: 4
+		}
+	},
+	position: {
+		google: { x: 0, y: 0 },
+		player1: { x: 3, y: 3 },
+		player2: { x: 2, y: 2 }
+	}
+}
 
-let observer = null;
+let observer = null
 
 export function subscribe(callback) {
-  observer = callback;
+	observer = callback
 }
 
 // getter / selector
 
 export function getStatus() {
-  return _state.status;
+	return _state.status
 }
 
 export function getGridSize() {
-  return _state.settings.gridSize;
+	return _state.settings.gridSize
 }
 
 export function getGooglePosition() {
-  return _state.position.google;
+	return _state.position.google
 }
 
 // setter / command /mutation / action
 
 export function startGame() {
-  _state.status = GAME_STATUSES.IN_PROGRESS;
+	_state.status = GAME_STATUSES.IN_PROGRESS
 
-  observer();
+	observer()
 
-  setInterval(() => {
-    _state.position.google.x++;
+	setInterval(() => {
+		_state.position.google.x++
 
-    observer();
-  }, 1000);
+		observer()
+	}, 1000)
 }
 
 // function getRandomInt(max) {
